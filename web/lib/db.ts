00123ef080ef4cdd74c95393073a735358719488
yousefh409 <yousefh409@gmail.com>
2026-05-27 18:43:01 -0700

    Disable SSL for Postgres internal connection

--- web/lib/db.ts
+++ web/lib/db.ts
@@ -5,13 +5,14 @@ declare global {
   var __pgPool: Pool | undefined;
 }
 
+const url = process.env.DATABASE_URL ?? "";
+const isInternal = url.includes(".railway.internal");
+
 export const pool =
   global.__pgPool ??
   new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL?.includes("railway")
-      ? { rejectUnauthorized: false }
-      : undefined,
+    connectionString: url,
+    ssl: isInternal ? false : { rejectUnauthorized: false },
   });
 
 if (process.env.NODE_ENV !== "production") global.__pgPool = pool;
